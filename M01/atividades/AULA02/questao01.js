3fd78499b451284d9affe9cd818368a09cca115f
Carlos Henrique <carlosh.sales@outlook.com>
2022-05-30 19:55:50 -0300

    Questões aula dois e tres

--- M01/atividades/AULA02/questao01.js
+++ M01/atividades/AULA02/questao01.js
@@ -4,3 +4,43 @@
  Baseado nos valores digitados, exiba a quantidade de turnos que o jogador 
  irá demorar para conseguir derrotar o monstro (o jogador irá derrotar o monstro em 8 turnos).
 */
+const prompt = require('prompt-sync')();
+console.clear();
+
+let hpBoss = +prompt('Qual a vida do boss? (entre 10 e 50 de hp): ');
+let verificado = true;
+let allHits = 0;
+
+do{
+    if(hpBoss < 10 || hpBoss > 50){
+        hpBoss = +prompt(`Valor fora dos limites 
+        Entre com valor entre 10 e 50.
+        Digite novamente:
+        `);
+    }else{
+        verificado = false;
+    }
+}while(verificado);
+
+
+let dmgPlayer = +prompt('Qual a quantidade de dano? (entre 5 e 10 dmg): ');
+verificado = true;
+
+do{
+    if(dmgPlayer < 5 || dmgPlayer > 10){
+        dmgPlayer = +prompt(`Valor fora dos limites 
+        Entre com valor entre 5 e 10.
+        Digite novamente:
+         `);
+    }else{
+        verificado = false;
+    }
+}while(verificado);
+
+while(hpBoss > 0){
+    hpBoss -= dmgPlayer;
+    allHits++;
+}
+
+console.log(`a quantidade de hits que precisou para matar o boss é de ${allHits}
+que causou ${dmgPlayer * allHits} dmg`);
---
+++ M01/atividades/AULA03/questao01.js
@@ -0,0 +1,12 @@
+/*
+Faça um programa que leia 2 valores inteiros (A e B). 
+Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", 
+indicando se os valores lidos são múltiplos entre si.
+*/
+
+let num1 = 12;
+let num2 = 3;
+
+if(num1 % num2 === 0 || num2 % num1 === 0){
+    console.log('deu');
+}
