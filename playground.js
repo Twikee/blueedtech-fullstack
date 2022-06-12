9350869e580c28cf291ba9867e8b7e7cd88ab17f
Carlos Henrique <carlosh.sales@outlook.com>
2022-06-12 19:48:22 -0300

    testes playground

--- playground.js
+++ playground.js
@@ -1,14 +1,10 @@
 const prompt = require('prompt-sync')();
 
-const condicoes = {
-    "linha-1": array[0][0] == array[0][1] && array[0][1] == array[0][2],
-    "linha-2": array[1][0] == array[1][1] && array[1][1] == array[1][2],
-    "linha-3": array[2][0] == array[2][1] && array[2][1] == array[2][2],
-    "coluna-1": array[0][0] == array[1][0] && array[1][0] == array[2][0],
-    "coluna-2": array[0][1] == array[1][1] && array[2][1] == array[0][1],
-    "coluna-3": array[0][2] == array[2][1] && array[2][1] == array[2][2],
-    "diagonal-1": array[0][0] == array[1][1] && array[1][1] == array[2][2],
-    "diagonal-2": array[2][0] == array[1][1] && array[1][1] == array[0][2],
-  };
+const array= [[1,2,3], [4,5,6], [7,8,9]];
 
-  console.log(consicoes.JSON);
+
+let valor = () => {
+  
+}
+
+console.log(valor);
